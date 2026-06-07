ee50e279508218e8da31a403c225c8da7ed604eb
yuzzi <lapshinartem0@gmail.com>
2026-06-07 16:56:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,23 @@ function getLocation() {
     let path = 'https://api.openweathermap.org/data/2.5/weather?lat=/' + latitude + '/' + longitude
     let link = document.querySelector('.link');
     link.innerHTML = '<a href="' + path + '">ваше местоположению</a>';
+    
     });
 
 }
 
 btn.addEventListener('click', getLocation);
+
+
+let map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+        })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([0, 0]),
+        zoom: 10
+    })
+});
