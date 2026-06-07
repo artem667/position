4741d2b217e600c9b8a453642f7a7327b0dd8ce6
yuzzi <lapshinartem0@gmail.com>
2026-06-07 16:48:15 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 navigator.geolocation.getCurrentPosition(function(position) {
     coords = position.coords;
     console.log(coords);
-});
+
 
 let latitude = coords.latitude;
 let longitude = coords.longitude;
@@ -9,3 +9,4 @@ let longitude = coords.longitude;
 let path = 'https://api.openweathermap.org/data/2.5/weather?lat=/' + latitude + '/' + longitude
 let link = document.querySelector('.link');
 link.innerHTML = '<a href="' + path + '">ваше местоположению</a>';
+});
