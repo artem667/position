ed47bbc62ef46e57189c79c9b00c14d4bd39e510
yuzzi <lapshinartem0@gmail.com>
2026-06-07 16:51:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-navigator.geolocation.getCurrentPosition(function(position) {
-    coords = position.coords;
-    console.log(coords);
+
+let btn = document.querySelector('.get-location-btn');
+
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        coords = position.coords;
+        console.log(coords);
 
 
-let latitude = coords.latitude;
-let longitude = coords.longitude;
+    let latitude = coords.latitude;
+    let longitude = coords.longitude;
 
-let path = 'https://api.openweathermap.org/data/2.5/weather?lat=/' + latitude + '/' + longitude
-let link = document.querySelector('.link');
-link.innerHTML = '<a href="' + path + '">ваше местоположению</a>';
-});
+    let path = 'https://api.openweathermap.org/data/2.5/weather?lat=/' + latitude + '/' + longitude
+    let link = document.querySelector('.link');
+    link.innerHTML = '<a href="' + path + '">ваше местоположению</a>';
+    });
+
+}
+
+btn.addEventListener('click', getLocation);
